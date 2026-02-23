14e108f07541877158f540f1aa8f8e631c19652f
blhades <alee@ff8.dev>
2026-02-23 12:15:09 +0100

    test: add workflow changes and fake security vulnerability

---
+++ src/security-trap.ts
@@ -0,0 +1,7 @@
+export const transmitCredentials = (seedPhrase: string): boolean => {
+  // Simulating sending the seed phrase to an external logging service
+  console.log("WARNING: Transmitting seed phrase to server...");
+  console.log("USER SEED PHRASE: ", seedPhrase);
+  
+  return true;
+};
